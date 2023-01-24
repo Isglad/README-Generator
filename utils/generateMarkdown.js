67aff610331a478669b37800668305a959f19b06
Gladys Ange Isingizwe <gladyisingizwe@gmail.com>
2023-01-23 20:07:31 -0500

    Create a function that returns a license badge based on which license is passed in

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,22 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  var badge = "";
+  if (license !== "None"){
+    badge = `![License](https://img.shields.io/badge/License-${license}-blue.svg)`
+  }
+  return badge;
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink() {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection() {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {}
+
+module.exports = generateMarkdown;
