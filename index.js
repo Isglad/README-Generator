73342035654e4f11ef2fd6592d972234a623d9c3
Gladys Ange Isingizwe <gladyisingizwe@gmail.com>
2023-01-23 20:18:55 -0500

    Include packages needed for README Generator application

---
+++ index.js
@@ -0,0 +1,14 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdown.js")
+
+// TODO: Create an array of questions for user input
+
+// TODO: Create a function to write README file
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
