99c7a99a6be076de8368ed65051dbabfd2c7ddcc
Gladys Ange Isingizwe <gladyisingizwe@gmail.com>
2023-01-23 20:19:48 -0500

    Add an array of questions for user input

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const generateMarkdown = require("./utils/generateMarkdown.js")
 
 // TODO: Create an array of questions for user input
 
+const questions = [
+    "What is the title of your project?",
+    "Please provide a short description of your project",
+    "Please provide installation instructions of your project and separate them by comma",
+    "Please provide usage information:",
+    "Please provide contribution guidelines:",
+    "Please provide test instructions:",
+    "Please choose a license for your application:",
+    "Please enter your GitHub username:",
+    "Please enter your email:"
+  ];
+
 // TODO: Create a function to write README file
 
 // TODO: Create a function to initialize app
